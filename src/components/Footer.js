25a2308c921b79f80f71dfdce39fe3bc9355c471
quazistellar <gni7777@yandex.ru>
2024-05-16 22:30:12 +0300

    who

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+ import React from "react";
+
+class Footer extends React.Component {
+    render() {
+        return (
+     <footer className="contacts" id="block5">
+      <div className="animated-text1">
+      <h1>© MPT 2024    *contacts*    responsible: DHRUVA</h1>
+      </div>
+      </footer>
+        );
+    }
+}
+
+export default Footer;
--- src/components/SectionOne.js
+++ src/components/SectionOne.js
@@ -1,4 +1,5 @@
 import React from "react";
+
 class SectionOne extends React.Component {
     render() {
         return (
